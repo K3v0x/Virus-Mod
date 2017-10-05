605f66b149a984a7ffd5266169ab755cc399afe0
smoroa14 <robertscho1240@gmail.com>
2017-10-05 15:19:11 +0200

    init

--- src/main/java/com/smoroa14_kevox/virusmod/VirusMod.java
+++ src/main/java/com/smoroa14_kevox/virusmod/VirusMod.java
@@ -15,27 +15,28 @@ public class VirusMod
     public static final String MODID = "virusmod";
     public static final String VERSION = "1.0";
 
+    @Mod.Instance
     public static VirusMod instance;
 
     @SidedProxy(clientSide = "com.smoroa14_kevox.virusmod.proxy.ClientProxy",
             serverSide = "com.smoroa14_kevox.virusmod.proxy.ServerProxy")
     public static CommonProxy proxy;
 
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        proxy.preInit(event);
+    }
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-     proxy.init();
+     proxy.init(event);
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
         proxy.postInit(event);
-    }
-
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        proxy.preInit(event);
     }
 }
