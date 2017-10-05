7e19135eac7ea1932b99b818b6d0c8335a440445
smoroa14 <robertscho1240@gmail.com>
2017-10-05 10:58:06 +0200

    add virus mod

---
+++ src/main/java/com/smoroa14_kevox/virusmod/VirusMod.java
@@ -0,0 +1,20 @@
+package com.smoroa14_kevox.virusmod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = VirusMod.MODID, version = VirusMod.VERSION)
+public class VirusMod
+{
+    public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+}
