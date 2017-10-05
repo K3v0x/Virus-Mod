54026e089e44bb45c9fc39b622ed87925ab4c800
smoroa14 <robertscho1240@gmail.com>
2017-10-05 15:15:12 +0200

    add grassvirus in common proxy

--- src/main/java/com/smoroa14_kevox/virusmod/proxy/CommonProxy.java
+++ src/main/java/com/smoroa14_kevox/virusmod/proxy/CommonProxy.java
@@ -2,9 +2,13 @@ package com.smoroa14_kevox.virusmod.proxy;
 
 
 import com.smoroa14_kevox.virusmod.VirusMod;
+import com.smoroa14_kevox.virusmod.blocks.GrassVirusBlock;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -15,6 +19,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber(modid = VirusMod.MODID)
 public class CommonProxy {
     //public static Block FLINT_STONE_BLOCK = new FlintstoneBlock("fredblock");
+    public static Block GRASS_VIRUS_BLOCK = new GrassVirusBlock("grassvirus");
 
     public void init(FMLInitializationEvent event){}
     public void preInit(FMLPreInitializationEvent event){}
@@ -24,22 +29,26 @@ public class CommonProxy {
     public static void registerBlocks(RegistryEvent.Register<Block> event)
     {
         //event.getRegistry().register(FLINT_STONE_BLOCK);
+        event.getRegistry().register(GRASS_VIRUS_BLOCK);
     }
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
         //event.getRegistry().register(new ItemBlock(FLINT_STONE_BLOCK).setRegistryName(FLINT_STONE_BLOCK.getRegistryName()));
+        event.getRegistry().register(new ItemBlock(GRASS_VIRUS_BLOCK).setRegistryName(GRASS_VIRUS_BLOCK.getRegistryName()));
     }
 
     @SubscribeEvent
     public static void registerRenderers(ModelRegistryEvent event)
     {
         //registerRenderer(Item.getItemFromBlock(FLINT_STONE_BLOCK));
+        registerRenderer(Item.getItemFromBlock(GRASS_VIRUS_BLOCK));
     }
 
     private static void registerRenderer(Item item)
     {
         //ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(),"inventory"));
+        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(),"inventory"));
     }
 }
