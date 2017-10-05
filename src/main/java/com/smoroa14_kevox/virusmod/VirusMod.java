76614b5a502707a0664c021d8e7696bf18fe99b6
Kevin Albrich <kevin.albrich@gmx.at>
2017-10-05 15:17:34 +0200

    Virus Mod class

--- src/main/java/com/smoroa14_kevox/virusmod/VirusMod.java
+++ src/main/java/com/smoroa14_kevox/virusmod/VirusMod.java
@@ -1,22 +1,41 @@
 package com.smoroa14_kevox.virusmod;
 
+import com.smoroa14_kevox.virusmod.proxy.CommonProxy;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = VirusMod.MODID, version = VirusMod.VERSION)
 public class VirusMod
 {
     public static final String MODID = "virusmod";
     public static final String VERSION = "1.0";
-    
+
+    public static VirusMod instance;
+
+    @SidedProxy(clientSide = "com.smoroa14_kevox.virusmod.proxy.ClientProxy",
+            serverSide = "com.smoroa14_kevox.virusmod.proxy.ServerProxy")
+    public static CommonProxy proxy;
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        System.out.println("");
+     proxy.init();
+    }
 
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        proxy.postInit(event);
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        proxy.preInit(event);
     }
 }
