692b20e75331f592812710f13b2ac39883e7b6e9
Kevin Albrich <kevin.albrich@gmx.at>
2017-10-05 21:28:55 +0200

    WaterVirus added

--- src/main/java/com/smoroa14_kevox/virusmod/proxy/ClientProxy.java
+++ src/main/java/com/smoroa14_kevox/virusmod/proxy/ClientProxy.java
@@ -1,7 +1,13 @@
 package com.smoroa14_kevox.virusmod.proxy;
 
+import jdk.nashorn.internal.ir.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 
 @Mod.EventBusSubscriber(Side.CLIENT)
@@ -9,15 +15,32 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void preInit(FMLPreInitializationEvent event) {
         super.preInit(event);
-        /*GameRegistry.addShapedRecipe(new ResourceLocation("Test"), null, new ItemStack(Items.EGG),
-                " p ",
-                " p ",
-                " s ",
-                'p', Items.PAPER,
-                's', Items.STICK);*/
+        GameRegistry.addShapedRecipe(new ResourceLocation("Virus Essence"), null, new ItemStack(Items.WATER_BUCKET),
+                "XXX",
+                "XOX",
+                "XXX",
+                'X', Items.SLIME_BALL,
+                'O', Items.NETHER_STAR);
 
+        GameRegistry.addShapedRecipe(new ResourceLocation("Water Virus"), null, new ItemStack(Blocks.MYCELIUM),
+                "XXX",
+                "XOX",
+                "XXX",
+                'X', Blocks.LEAVES,
+                'O',Items.WHEAT_SEEDS);
 
+        GameRegistry.addShapedRecipe(new ResourceLocation("Grass Virus"), null, new ItemStack(Items.WATER_BUCKET),
+                "XXX",
+                "XOX",
+                "XXX",
+                'X', Blocks.LAPIS_ORE,
+                'O', Items.WATER_BUCKET);
 
-
+        GameRegistry.addShapedRecipe(new ResourceLocation("Lava Virus"), null, new ItemStack(Items.LAVA_BUCKET),
+                "XXX",
+                "XOX",
+                "XXX",
+                'X', Items.MAGMA_CREAM,
+                'O', Items.LAVA_BUCKET);
     }
 }
