25c6ab50d33ed9fca26b80b068f5f516100eef80
smoroa14 <robertscho1240@gmail.com>
2017-10-06 14:53:04 +0200

    wasservirus weitergemacht

--- src/main/java/com/smoroa14_kevox/virusmod/engine/PlaceWater.java
+++ src/main/java/com/smoroa14_kevox/virusmod/engine/PlaceWater.java
@@ -24,32 +24,39 @@ public class PlaceWater implements Runnable {
     @Override
     public void run() {
         IBlockState bs = world.getBlockState(new BlockPos(x+1, y, z));
-
-        threads(bs,x, y, z);
+        Thread[] ts = new Thread[4];
+        threads(bs,x, y, z, ts, 0);
         bs = world.getBlockState(new BlockPos(x-1, y, z));
 
-        threads(bs,x, y, z);
+        threads(bs,x, y, z, ts, 1);
         bs = world.getBlockState(new BlockPos(x, y, z+1));
 
-        threads(bs,x, y, z);
+        threads(bs,x, y, z, ts, 2);
         bs = world.getBlockState(new BlockPos(x, y, z-1));
-        threads(bs,x, y, z);
+        threads(bs,x, y, z, ts, 3);
+
+        for (Thread t : ts) {
+            if(t != null)
+            {
+                t.start();
+                CommonProxy.WATER_VIRUS_BUCKET.notify();
+                try {
+                    CommonProxy.WATER_VIRUS_BUCKET.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
-    public void threads(IBlockState bs, int x, int y, int z)
+    public void threads(IBlockState bs, int x, int y, int z, Thread[] ts, int pos)
     {
         bs = world.getBlockState(new BlockPos(x, y, z-1));
         if(bs.getBlock() == Blocks.AIR)
         {
             world.setBlockState(new BlockPos(x, y, z-1), CommonProxy.GRASS_VIRUS_BLOCK.getDefaultState());
             Thread t = new Thread(new PlaceWater(world, x, y, z-1));
-            t.start();
-            CommonProxy.WATER_VIRUS_BUCKET.notify();
-            try {
-                CommonProxy.WATER_VIRUS_BUCKET.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            ts[pos] = t;
         }
     }
 }
